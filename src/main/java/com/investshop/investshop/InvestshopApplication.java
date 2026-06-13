f978772ffe1158595f699e0180903ffbfb8572f0
Ayaelhassani <elhassaniaya02@gmail.com>
2026-06-13 23:24:32 +0200

    new project

---
+++ src/main/java/com/investshop/investshop/InvestshopApplication.java
@@ -0,0 +1,13 @@
+package com.investshop.investshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvestshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/investshop/investshop/InvestshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.investshop.investshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
